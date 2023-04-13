f1bded8dd05487be66c0f1d436828539604e1cc6
sven <sven2000@student.miun.se>
2023-04-13 14:01:09 +0200

    Moved from doxygen to here

---
+++ other.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "ar.hpp"
+#include "opts.hpp"
+
+using std::string;
+
+int main(void) {
+  std::string token = "pnts_ArrayAvgSumNumHeelObvs";
+  token = "getColor()";
+  std::string res = ar::do_ar(token);
+  std::cout << "result: " << res << std::endl;
+  return 0;
+}
